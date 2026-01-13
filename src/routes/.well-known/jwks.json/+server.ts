f2e22e8e8faee4dd3f329710a60ec2104d62e75d
Simon Green Kristensen <git@simon-green.dev>
2026-01-14 00:07:30 +0100

    fat: add jwks json

---
+++ src/routes/.well-known/jwks.json/+server.ts
@@ -0,0 +1,26 @@
+import { JWKS_ID, JWKS_KEY } from '$env/static/private';
+
+export function GET() {
+	return new Response(
+		JSON.stringify({
+			keys: [
+				{
+					alg: 'RS256',
+					e: 'AQAB',
+					ext: true,
+					key_ops: ['verify'],
+					kty: 'RSA',
+					n: JWKS_KEY,
+					kid: JWKS_ID,
+					use: 'sig',
+				},
+			],
+		}),
+		{
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		},
+	);
+}
+export const prerender = true;
