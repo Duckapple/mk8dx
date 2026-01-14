c4bec141be38d3432920f0e53ef117b7c031e249
Simon Green Kristensen <git@simon-green.dev>
2026-01-14 01:49:23 +0100

    fix: single key jwks

--- src/routes/.well-known/jwks.json/+server.ts
+++ src/routes/.well-known/jwks.json/+server.ts
@@ -3,18 +3,14 @@ import { JWKS_ID, JWKS_KEY } from '$env/static/private';
 export function GET() {
 	return new Response(
 		JSON.stringify({
-			keys: [
-				{
-					alg: 'RS256',
-					e: 'AQAB',
-					ext: true,
-					key_ops: ['verify'],
-					kty: 'RSA',
-					n: JWKS_KEY,
-					kid: JWKS_ID,
-					use: 'sig',
-				},
-			],
+			alg: 'RS256',
+			e: 'AQAB',
+			ext: true,
+			key_ops: ['verify'],
+			kty: 'RSA',
+			n: JWKS_KEY,
+			kid: JWKS_ID,
+			use: 'sig',
 		}),
 		{
 			headers: {
